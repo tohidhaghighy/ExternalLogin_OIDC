6f2dd666af9eb2e73869dff25e8f1362a5ceccea
tohidhaghighi@gmail.com <tohidhaghighi@gmail.com>
2023-04-15 22:03:23 -0700

    a

---
+++ ExternalLogin/Controllers/GoogleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExternalLogin.Controllers
+{
+    [Route("signin-google")]
+    public class GoogleController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
